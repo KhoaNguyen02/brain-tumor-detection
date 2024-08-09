0d8a28eb6724c05aa4d6395c69d583c0721f1d61
Nguyen Anh Khoa <anhkhoa.nguyen@ru.nl>
2024-08-09 11:07:32 +0700

    Resize input image

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ def main():
     transform = transforms.Compose([
         transforms.ToPILImage(),
         transforms.ToTensor(),
+        transforms.Resize((224, 224)),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[
                             0.229, 0.224, 0.225])
     ])
