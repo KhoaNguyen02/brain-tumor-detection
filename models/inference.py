df67e054f060505c06cb8472190f34911280bd44
Nguyen Anh Khoa <anhkhoa.nguyen@ru.nl>
2024-08-23 15:58:44 +0700

    Remove redundant code

--- models/inference.py
+++ models/inference.py
@@ -1,5 +1,3 @@
-import cv2
-import numpy as np
 import torch
 import torch.nn.functional as F
 
@@ -37,71 +35,3 @@ def predict(model, image, device):
     max_confidence = class_probabilities[max_class]
 
     return max_class, max_confidence, class_probabilities
-
-def generate_cam(model, input_data):
-    """Generates class activation maps (CAM) for a given model and input data.
-
-    Parameters
-    ----------
-    model : torch.nn.Module
-        Model to generate CAM for
-    input_data : torch.Tensor or torch.utils.data.DataLoader
-        Input data to generate CAM for (single image or DataLoader)
-
-    Returns
-    -------
-    list of np.ndarray
-        List of CAMs for the input data
-    """
-
-    def process_single_image(image):
-        model.eval()
-        pred = model(image, inference=True)
-        cls = pred.argmax(dim=1)
-
-        pred[:, cls].backward()
-        gradients = model.get_gradient()
-        pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
-        activations = model.get_activations(image).detach()
-
-        for i in range(activations.shape[1]):
-            activations[:, i, :, :] *= pooled_gradients[i]
-
-        heatmap = torch.mean(activations, dim=1).squeeze()
-        heatmap = np.maximum(heatmap.cpu(), 0)
-        heatmap /= torch.max(heatmap)
-        heatmap = heatmap.numpy()
-
-        return heatmap
-
-    if isinstance(input_data, torch.utils.data.DataLoader):
-        heatmaps = []
-        for images in input_data:
-            for image in images:
-                heatmap = process_single_image(image.unsqueeze(0))
-                heatmaps.append(heatmap)
-        return heatmaps
-    else:
-        return process_single_image(input_data)
-
-def apply_heatmap(img, heatmap):
-    """Applies a heatmap to an image. Useful for visualizing CAMs.
-
-    Parameters
-    ----------
-    img : PIL.Image or np.ndarray
-        Image to apply the heatmap to
-    heatmap : np.ndarray
-        Heatmap to apply to the image
-
-    Returns
-    -------
-    np.ndarray
-        Image with the heatmap applied
-    """
-    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
-    heatmap = np.uint8(255 * heatmap)
-    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
-
-    superimposed_img = heatmap * 0.4 + img
-    return superimposed_img
